92d6dc736d982e0f83b1ccebc011f4bf03113d20
Maciej <mackolinop@gmail.com>
2024-11-13 19:24:45 +0100

    First steps

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+run = True
+
+while run:
+    print("Welcome to the Console Games!")
+    print("What would you like to do?")
+    print("1. Tic tac toe\n2. Snake\n3. Tetris\n4. Quit")
+
+    choice = input("Your choice: ")
+
+    if choice == "1":
+        print("Tic tac toe")
+
+    elif choice == "2":
+        print("Snake")
+
+    elif choice == "3":
+        print("Tetris")
+
+    elif choice == "4":
+        run = False
+
+    else:
+        print("Wrong input")
